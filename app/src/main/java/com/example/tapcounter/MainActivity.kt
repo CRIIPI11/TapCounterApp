c3067ab4334fa10b611ed1f470a8433605df20c7
criipi <cdml2001@hotmail.com>
2023-01-26 22:05:08 -0500

    made button functional

--- app/src/main/java/com/example/tapcounter/MainActivity.kt
+++ app/src/main/java/com/example/tapcounter/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.tapcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val but = findViewById<Button>(R.id.button)
+        val text = findViewById<TextView>(R.id.counter)
+
+        but.setOnClickListener{
+            counter++
+            text.text = counter.toString()
+        }
+
     }
 }
