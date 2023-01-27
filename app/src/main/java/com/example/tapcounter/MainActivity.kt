62429481cda4b9a63ba3ead1d4321d0879f56c59
cristhian <cdml2001@hotmail.com>
2023-01-27 11:41:59 -0500

    added customization

--- app/src/main/java/com/example/tapcounter/MainActivity.kt
+++ app/src/main/java/com/example/tapcounter/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.tapcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 
 class MainActivity : AppCompatActivity() {
     var counter = 0
+    var openLayout = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(applicationContext, "Kill All The Zombies!", Toast.LENGTH_SHORT).show()
+
 
         val but = findViewById<Button>(R.id.button)
         val text = findViewById<TextView>(R.id.counter)
@@ -21,4 +26,8 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+
+
+
 }
