8f1a1e77c7840e54bc7c27ae53038b29ac46261f
cristhian <cdml2001@hotmail.com>
2023-01-26 22:40:51 -0500

    fixed code

--- app/src/main/java/com/example/tapcounter/MainActivity.kt
+++ app/src/main/java/com/example/tapcounter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.tapcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
